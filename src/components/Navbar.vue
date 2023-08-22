c858a3fa1c5d052b185b32dbad2c55bfe069b60d
danis <sharafiev@zvonok.com>
2023-08-22 18:19:45 +0300

    nothing

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container fluid>
+      <v-app-bar color='pink' dense black>
+        <v-spacer></v-spacer>
+         <v-btn outlined class="ml-0"> Главная </v-btn>
+        <v-btn outlined class="ml-5"> Фотографии </v-btn>
+      </v-app-bar>
+    </v-container>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style scoped>
+
+</style>
